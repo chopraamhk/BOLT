49c48c1cc354575312a25e627435368372bd2ec7
Mehak Chopra <m.chopra1@universityofgalway.ie>
2024-07-03 12:20:55 +0100

    Update bolt.sh

--- bolt.sh
+++ bolt.sh
@@ -29,7 +29,7 @@
         --covarFile=bolt_covars.txt \
         --sampleFile=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/bgen/chr1.sample \
         --bgenFile=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/bgen/chr${SLURM_ARRAY_TASK_ID}.bgen \
-        --statsFileBgenSnps=results/ao_bgen_${SLURM_ARRAY_TASK_ID}.stats \
+        --statsFileBgenSnps=results/ao_${SLURM_ARRAY_TASK_ID}.stats \
         --bgenMinMAF=1e-3 \
         --bgenMinINFO=0.3 \
         --LDscoresMatchBp \
@@ -38,3 +38,9 @@
         --numLeaveOutChunks 2 \
         --covarUseMissingIndic \
         --maxModelSnps 4000000
+
+
+#awk 'NR==1{next} FNR==1 && NR!=1{next} 1' ao_1.stats ao_2.stats ao_3.stats ao_4.stats ao_5.stats ao_6.stats ao_7.stats ao_8.stats ao_9.stats ao_10.stats ao_11.stats ao_12.stats ao_13.stats ao_14.stats ao_15.stats ao_16.stats ao_17.stats ao_18.stats ao_19.stats ao_20.stats ao_21.stats ao_22.stats > ao_combined.stats
+
+#make sure to add the below as header 
+#SNP	CHR	BP	GENPOS	ALLELE1	ALLELE0	A1FREQ	INFO	CHISQ_LINREG	P_LINREG	BETA	SE	CHISQ_BOLT_LMM_INF	P_BOLT_LMM_INF

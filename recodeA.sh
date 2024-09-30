031e966a555d8d2f94edd9af38b20fdeb9b637c5
Mehak Chopra <m.chopra1@universityofgalway.ie>
2024-09-30 16:42:55 +0100

    Update recodeA.sh

--- recodeA.sh
+++ recodeA.sh
@@ -14,3 +14,7 @@ conda activate plink
 
 ##The --recodeAD option produces both an additive and dominance coding: use --recodeA instead to skip the SNP_HET coding.
 plink --bfile ld_1_22 --recodeA --out recodeA
+
+
+##head -n 5 recodeA.raw | cut -d ' ' -f 1-10
+##if want to see the header

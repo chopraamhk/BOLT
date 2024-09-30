f9cba7b6f75f4b1f09e35f203ef93667ea72a6f7
Mehak Chopra <m.chopra1@universityofgalway.ie>
2024-09-30 16:30:06 +0100

    Create recodeA.sh

---
+++ recodeA.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name="recodeA"
+#SBATCH -o recode_A.o%j
+#SBATCH -e recode_A.e%j
+#SBATCH --mail-user=m.chopra1@universityofgalway.ie
+#SBATCH --mail-type=ALL
+#SBATCH --partition="highmem"
+#SBATCH -n 32
+#SBATCH -N 1
+
+module load anaconda3/2024.02-1
+conda activate plink
+
+##The --recodeAD option produces both an additive and dominance coding: use --recodeA instead to skip the SNP_HET coding.
+plink --bfile ld_1_22 --recodeA --out recodeA
